181a79811f3d15cbd51419261d74fc6b5ba8b48b
Jake McCrary <jake@jakemccrary.com>
2011-09-22 16:55:07 -0500

    multiple balls move around

--- balls.js
+++ balls.js
@@ -1,43 +1,5 @@
-
 var max_x = 1000;
 var max_y = 300;
-
-function circle(center, context) {
-  var that = {};
-  var dx = randInt(10);
-  var dy = randInt(10);
-
-  function makeCircle() {
-    context.arc(center.x, center.y, 50, 0, Math.PI*2, false);
-    context.closePath();
-    var color = randomColor();
-//    var color = "blue";
-    context.strokeStyle = color;
-    context.fillStyle = color;
-  };
-
-  that.draw = function() {
-    context.beginPath();
-    makeCircle();
-    context.stroke();
-    context.fill()
-  };
-
-  that.move = function() {
-    if (center.x >= max_x || center.x < 0) {
-      dx = -dx;
-    }
-    if (center.y >= max_y || center.y < 0) {
-      dy = -dy;
-    }
-    center.x += dx;
-    center.y += dy;
-//    context.clearRect(0, 0, max_x, max_y);
-    that.draw();
-  };
-  
-  return that;
-}
 
 function point(x, y) {
   p = {};
@@ -59,6 +21,38 @@ function randomPoint() {
   return point(randInt(max_x), randInt(max_y));
 }
 
+function circle(center, context) {
+  var that = {};
+  var dx = randInt(10);
+  var dy = randInt(10);
+  var color = randomColor();
+  var size =  randInt(50) + 10;
+
+  that.draw = function() {
+    context.beginPath();
+    context.arc(center.x, center.y, size, 0, Math.PI*2, false);
+    context.strokeStyle = color;
+    context.fillStyle = color;
+    context.closePath();
+    context.stroke();
+    context.fill()
+  };
+
+  that.move = function() {
+    if (center.x >= max_x || center.x < 0) {
+      dx = -dx;
+    }
+    if (center.y >= max_y || center.y < 0) {
+      dy = -dy;
+    }
+    center.x += dx;
+    center.y += dy;
+    that.draw();
+  };
+
+  return that;
+}
+
 function initGame() {
   canvasElement = document.createElement("canvas");
   canvasElement.id = "a";
@@ -67,10 +61,19 @@ function initGame() {
 
   document.body.appendChild(canvasElement);
 
-  c = circle(randomPoint(), canvasElement.getContext("2d"));
-  c.draw(0, 0);
+  var context = canvasElement.getContext("2d");
+  var num = 10;
+  var circles = [];
+  for (var i = 0; i < num; i++) {
+    circles[i] = circle(randomPoint(), context);
+  }
   setInterval(function() {
-    c.move();
+    context.clearRect(0, 0, max_x, max_y);
+
+    for (var i = 0; i < num; i++) {
+      circles[i].move();
+    }
+
   }, 16);
 }
 
