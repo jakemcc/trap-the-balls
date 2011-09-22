671e56af1f8c792666a606cdd173430160f59b03
Jake McCrary <jake@jakemccrary.com>
2011-09-22 16:26:22 -0500

    Ball now bounces around canvas. canvas created in javascript

--- balls.js
+++ balls.js
@@ -1,23 +1,42 @@
 
+var max_x = 1000;
+var max_y = 300;
 
-function canvas(context) {
-  var a_context = context;
+function circle(context) {
   var that = {};
-  that.context = a_context;
-  that.drawCircle = function(x, y) {
-    a_context.beginPath();
-    a_context.arc(x, y, 50, 0, Math.PI*2, false);
-    a_context.closePath();
-    //  a_context.strokeStyle = "#000";
-    a_context.stroke();
-    a_context.fillStyle = randomColor();
-    a_context.fill()
+  var dx = 10;
+  var dy = 10;
+
+  that.makeCircle = function(x, y) {
+    that.x = x;
+    that.y = y;
+    context.arc(x, y, 50, 0, Math.PI*2, false);
+    context.closePath();
+    var color = randomColor();
+    context.strokeStyle = color;
+    context.fillStyle = color;
   }
+
+  that.draw = function(x, y) {
+    context.beginPath();
+    that.makeCircle(x, y);
+    context.stroke();
+    context.fill()
+  };
+
+  that.move = function() {
+    if (that.x >= max_x || that.x < 0) {
+      dx = -dx;
+    }
+    if (that.y >= max_y || that.y < 0) {
+      dy = -dy;
+    }
+    context.clearRect(0, 0, max_x, max_y);
+    that.draw(that.x + dx, that.y + dy);
+
+  };
   return that;
 }
-
-var max_x;
-var max_y;
 
 function point(x, y) {
   p = {};
@@ -29,6 +48,7 @@ function point(x, y) {
 function randInt(upper_bound) {
   return Math.floor(Math.random() * (upper_bound + 1));
 }
+
 var colors = ["red", "green", "blue", "brown", "pink", "lightgreen", "goldenrod"];
 function randomColor() {
   return colors[randInt(colors.length)];
@@ -38,16 +58,19 @@ function randomPoint() {
   return point(randInt(max_x), randInt(max_y));
 }
 
-var env;
 function initGame() {
-  var a_canvas = document.getElementById("a");
-  max_x = a_canvas.width;
-  max_y = a_canvas.height;
-  env = canvas(a_canvas.getContext("2d"));
+  canvasElement = document.createElement("canvas");
+  canvasElement.id = "a";
+  canvasElement.width = max_x;
+  canvasElement.height = max_y;
+
+  document.body.appendChild(canvasElement);
+
+  c = circle(canvasElement.getContext("2d"));
+  c.draw(0, 0);
   setInterval(function() {
-    var p = randomPoint();
-    env.drawCircle(p.x, p.y);
-  }, 100);
+    c.move(5, 1)
+  }, 16);
 }
 
 
