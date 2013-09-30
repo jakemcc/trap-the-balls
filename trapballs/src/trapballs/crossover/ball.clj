0e83f67a27eb4d35659d0b7de6d39dc6a6f323b4
Jake McCrary <jake@jakemccrary.com>
2013-09-29 18:56:22 -0700

    remove time component from collision detection

--- trapballs/src/trapballs/crossover/ball.clj
+++ trapballs/src/trapballs/crossover/ball.clj
@@ -7,14 +7,13 @@
       (update-in [:x] #(+ % (* (:dx ball) (/ time 1000))))
       (update-in [:y] #(+ % (* (:dy ball) (/ time 1000))))))
 
-(defn collide [time ball space]
-  (let [time-frac (/ time 1000)]
-    (-> ball
-        (update-in [:dx] #(if (or (>= (:x ball) (:max-x space))
-                                  (<= (:x ball) (:origin-x space)))
-                            (- %)
-                            %))
-        (update-in [:dy] #(if (or (>= (:y ball) (:max-y space))
-                                  (<= (:y ball) (:origin-y space)))
-                            (- %)
-                            %)))))
+(defn collide [ball space]
+  (-> ball
+      (update-in [:dx] #(if (or (>= (:x ball) (:max-x space))
+                                (<= (:x ball) (:origin-x space)))
+                          (- %)
+                          %))
+      (update-in [:dy] #(if (or (>= (:y ball) (:max-y space))
+                                (<= (:y ball) (:origin-y space)))
+                          (- %)
+                          %))))
--- trapballs/test/trapballs/crossover/ball_expectations.clj
+++ trapballs/test/trapballs/crossover/ball_expectations.clj
@@ -6,16 +6,14 @@
         (move (->Ball 4 9 1 2) 1000))
 
 (expect (->Ball 6 5 -1 0)
-        (collide 1000
-                 (->Ball 6 5 1 0)
+        (collide (->Ball 6 5 1 0)
                  {:origin-x 0
                   :origin-y 0
                   :max-x 6
                   :max-y 6}))
 
 (expect (->Ball 5 6 0 -1)
-        (collide 1000
-                 (->Ball 5 6 0 1)
+        (collide (->Ball 5 6 0 1)
                  {:origin-x 0
                   :origin-y 0
                   :max-x 6
