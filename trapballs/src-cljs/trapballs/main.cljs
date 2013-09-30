acdbcf72f2f0d4cd3ec95c5dd60179d0fe6161c7
Jake McCrary <jake@jakemccrary.com>
2013-09-29 19:42:13 -0700

    balls bounce (poorly) and move around

--- trapballs/src-cljs/trapballs/main.cljs
+++ trapballs/src-cljs/trapballs/main.cljs
@@ -12,6 +12,18 @@
      (.-width canvas)
      (.-height canvas)]))
 
+(defn surface [canvas]
+  (first canvas))
+
+(defn context [canvas] :^CanvasRenderingContext2D
+  (second canvas))
+
+(defn width [canvas]
+  (nth canvas 2))
+
+(defn height [canvas]
+  (nth canvas 3))
+
 (defn set-border [elem]
   (.setAttribute elem "style" "border:1px solid #000"))
 
@@ -26,9 +38,9 @@
   (draw [ball context]
     (.save context)
     (.beginPath context)
-    (set! (.-strokeStyle context) "blue")
-    (set! (.-fillStyle context) "blue")
-    (.arc context (:x ball) (:y ball) 3, 0, (* 2 Math/PI), false)
+    (set! (.-strokeStyle context) (get ball :color "blue"))
+    (set! (.-fillStyle context) (get ball :color "blue"))
+    (.arc context (:x ball) (:y ball) 10, 0, (* 2 Math/PI), false)
     (.closePath context)
     (.stroke context)
     (.fill context)
@@ -42,20 +54,36 @@
 (defn current-time []
   (.getTime (js/Date.)))
 
-(defn init-state [context]
-  {:context context
-   :balls [(trapballs.crossover.ball/->Ball 30 30 20 3)]})
+
+(defn move-balls [elapsed-time balls]
+  (mapv (fn [ball] (b/move ball elapsed-time))
+        balls))
+
+(defn collide-balls [balls space]
+  (mapv (fn [ball] (b/collide ball space)) balls))
+
+(defn space [state]
+  {:origin-x 0
+   :origin-y 0
+   :max-x (width (:on-screen-canvas state))
+   :max-y (height (:on-screen-canvas state))})
 
 (defn physics [elapsed-time state]
-  (let [updated-balls (mapv (fn [ball] (b/move ball elapsed-time))
-                            (:balls state))]
+  (let [updated-balls (move-balls elapsed-time (:balls state))
+        updated-balls (collide-balls updated-balls (space state))
+        ]
     (merge state {:balls updated-balls})))
 
 (defn render [state]
-  (let [context (:context state)]
-    (.clearRect context 0 0 600 300)
+  (let [on-screen-canvas (:on-screen-canvas state)
+        on-screen-context (context on-screen-canvas)
+        off-screen (:off-screen-canvas state)
+        off-screen-context (context off-screen)]
+    (.clearRect on-screen-context 0 0 (width on-screen-canvas) (height on-screen-canvas))
+    (.clearRect off-screen-context 0 0 (width on-screen-canvas) (height on-screen-canvas))
     (doseq [ball (:balls state)]
-      (draw ball context))))
+      (draw ball off-screen-context))
+    (.drawImage on-screen-context (surface off-screen) 0 0)))
 
 (defn step [elapsed-time state]
   (let [new-state (physics elapsed-time state)]
@@ -74,10 +102,28 @@
     (.requestAnimationFrame js/window (stepper timestamp
                                                step-fn
                                                state))))
+
+(defn num-between [x y]
+  (+ x (rand-int y)))
+
+(defn init-state [on-screen-canvas off-screen-canvas]
+  {:on-screen-canvas on-screen-canvas
+   :off-screen-canvas off-screen-canvas
+   :balls (for [_ (range 100)]
+            (assoc
+                (trapballs.crossover.ball/->Ball
+                 (num-between 200 400)
+                 (num-between 100 100)
+                 (num-between -50 50)
+                 (num-between -50 50))
+              :color (rand-nth ["red" "green" "blue" "brown" "pink" "lightgreen" "goldenrod" "purple"])))})
+
 (defn ^:export main []
-  (let [[surface context] (make-canvas "onscreen")
-        state (init-state context)]
-    (set-border surface)
-    (dom/appendChild (body) surface)
+  (let [on-screen-canvas (make-canvas "onscreen")
+        off-screen-canvas (make-canvas "offscreen")
+        state (init-state on-screen-canvas
+                          off-screen-canvas)]
+    (set-border (surface on-screen-canvas))
+    (dom/appendChild (body) (surface on-screen-canvas))
     (.requestAnimationFrame js/window (start-stepping step
                                                       state))))
